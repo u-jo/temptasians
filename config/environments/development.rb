0acb4086cd9bb00334e8937b1f0443fdb71669a9
u-jo <lee.yuzhou@gmail.com>
2015-01-03 00:16:31 -0500

    Compiles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  # 
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+  config.assets.precompile += %w( *.eot *.woff *.ttf *.otf *.svg ) 
 end
---
+++ public/assets/templates/image-hover-40f6ecc65db1b113330a08c201421a8b.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".icon-overlay a").prepend('<span class="icn-more"></span>')});
